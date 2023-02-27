1ffcb29feaec67744d3724e189be80a93d276a5c
DimonKniaziev <dimkniazz@gmail.com>
2023-02-27 11:59:58 +0300

    adding-redirect-for-secret-page

--- src/components/pages/secret-page.js
+++ src/components/pages/secret-page.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const SecretPage = ({isLoggedIn}) => {
-    if(isLoggedIn) {
-        return (
-            <div>
-                <h3>This Page is Full of Secrets!</h3>
-            </div>
-        );
+    if(!isLoggedIn) {
+        return <Navigate to="/login"/>
     }
+    return (
+        <div>
+            <h3>This Page is Full of Secrets!</h3>
+        </div>
+    );
 }
 export default SecretPage;
